ec0745d25be14664972ced86ab27c5c35e26c003
Nadeŭka <nadevko@riseup.net>
2023-10-10 18:39:08 +0300

    gjs for desktop icons extension

--- nixos/lib/desktop.nix
+++ nixos/lib/desktop.nix
@@ -5,7 +5,10 @@ with lib; {
       enable = true;
       libinput.enable = true;
       displayManager.gdm.enable = true;
-      desktopManager.gnome.enable = true;
+      desktopManager.gnome = {
+        extraGSettingsOverridePackages = with pkgs; [ gnome.nautilus ];
+        enable = true;
+      };
       excludePackages = with pkgs; [ xterm ];
     };
     pipewire = {
@@ -34,6 +37,7 @@ with lib; {
       gnome.nautilus-python
       gnome.gnome-tweaks
       glib.dev
+      gjs
     ];
     gnome.excludePackages = (with pkgs; [ gnome-tour gnome-photos ])
       ++ (with pkgs.gnome; [ cheese epiphany yelp gnome-maps ]);
