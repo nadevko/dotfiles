9f822b1a55f4f06d45de172f1a69017166523584
Nadeŭka <nadevko@riseup.net>
2023-10-18 23:29:19 +0300

    Replace wine waylandFull with stable

--- nixos/lib/virtualisation.nix
+++ nixos/lib/virtualisation.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
-    wineWowPackages.waylandFull
+    wineWowPackages.stable
+    # wineWowPackages.waylandFull
     winetricks
   ];
   virtualisation = {
