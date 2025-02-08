00d7cb0298e08319163a40069fa30cd7e09cfd21
Nadeŭka <nadevko@riseup.net>
2025-02-08 17:39:55 +0300

    Make lib be able to accept either pkgs or pkgs.lib

--- default.nix
+++ default.nix
@@ -7,38 +7,45 @@ let
     substring
     listToAttrs
     attrNames
+    filter
     ;
   inherit (trivial) loadDir;
-  mapAttrs' = f: set: listToAttrs (map (attr: f attr set.${attr}) (attrNames set));
-  removeSuffix =
-    suffix: str:
-    let
-      sufLen = stringLength suffix;
-      sLen = stringLength str;
-    in
-    if sufLen > sLen then
-      str
-    else if suffix == substring (sLen - sufLen) sufLen str then
-      substring 0 (sLen - sufLen) str
-    else
-      str;
   fix =
     f:
     let
       x = f x;
     in
     x;
-  trivial = fix (import ./lib/trivial.nix) (
-    builtins
-    // {
-      inherit mapAttrs' removeSuffix;
-    }
-  );
+
+  trivial = fix (import ./lib/trivial.nix) {
+    lib = builtins // {
+      mapAttrs' = f: set: listToAttrs (map (attr: f attr set.${attr}) (attrNames set));
+      removeSuffix =
+        suffix: str:
+        let
+          sufLen = stringLength suffix;
+          sLen = stringLength str;
+        in
+        if sufLen <= sLen && suffix == substring (sLen - sufLen) sufLen str then
+          substring 0 (sLen - sufLen) str
+        else
+          str;
+      filterAttrs = pred: set: removeAttrs set (filter (name: !pred name set.${name}) (attrNames set));
+      hasSuffix =
+        suffix: content:
+        let
+          lenContent = stringLength content;
+          lenSuffix = stringLength suffix;
+        in
+        lenContent >= lenSuffix && substring (lenContent - lenSuffix) lenContent content == suffix;
+    };
+  };
+
   libOverlay = final: prev: loadDir (dir: import dir final prev) ./lib;
 in
 (loadDir (dir: pkgs.callPackage dir { }) ./pkgs)
 // {
-  lib = fix libOverlay pkgs.lib;
+  lib = fix libOverlay pkgs;
   modules = loadDir (dir: import dir) ./modules;
   overlays = {
     lib = libOverlay;
--- lib/trivial.nix
+++ lib/trivial.nix
@@ -1,8 +1,8 @@
-final: prev: with prev; {
+final: prev: with (if builtins.hasAttr "lib" prev then prev.lib else prev); {
   loadDir =
     f: dir:
     mapAttrs' (k: v: {
       name = removeSuffix ".nix" k;
       value = f /${dir}/${k};
-    }) (readDir dir);
+    }) (filterAttrs (k: v: hasSuffix ".nix" k || v == "directory") (readDir dir));
 }
