94c7eaca8a0959d2c0dc17060f64eda4f0eeb22b
Nadeŭka <me@nadevko.cc>
2025-10-10 21:19:41 +0300

    add commit hook

--- home/configurations/nadevko/system.nix
+++ home/configurations/nadevko/system.nix
@@ -1,17 +1,20 @@
 { inputs, pkgs, ... }:
 {
-  home.packages = with pkgs; with inputs.self.packages.${pkgs.system}; [
-    atool
-    ayugram-desktop
-    gimp3-with-plugins
-    libreoffice-fresh
-    # nexusmods-app-unfree
-    obsidian
-    pandoc
-    rmlint
-    staruml
-    zotero
-  ];
+  home.packages =
+    with pkgs;
+    with inputs.self.packages.${pkgs.system};
+    [
+      atool
+      ayugram-desktop
+      gimp3-with-plugins
+      libreoffice-fresh
+      # nexusmods-app-unfree
+      obsidian
+      pandoc
+      rmlint
+      staruml
+      zotero
+    ];
   home = {
     stateVersion = "25.05";
     username = "nadevko";
