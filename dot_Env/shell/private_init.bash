02113501a56108fd671ddad098e73130fe9700df
nadevko <me+oss@nadevko.cc>
2026-06-23 03:23:09 +0300

    add layouts

--- dot_Env/shell/private_init.bash
+++ dot_Env/shell/private_init.bash
@@ -1,13 +1,15 @@
 # man 1 bash
 eval "$(direnv hook bash)"
+eval "$(fzf --bash)"
 
 set -a
 CDPATH=.:~/Workspace
 GLOBSORT=-mtime
+CHEZMOI_SOURCE_PATH=~/.Profile
 set +a
 
 shopt -s autocd cdspell dirspell
-shopt -s extglob globstar progcomp_alias
+shopt -s extglob globstar
 shopt -s bash_source_fullpath checkjobs lastpipe
 shopt -s gnu_errfmt xpg_echo checkwinsize
 shopt -s histappend histreedit histverify cmdhist
@@ -21,11 +23,11 @@ alias l='ls -lA'
 alias spm='sudo emerge'
 alias spmw='sudo emerge -uaND @world'
 
-alias zl='chezmoi status'
-alias za='chezmoi add'
-alias zar='chezmoi re-add'
-alias zs='chezmoi apply'
-alias zi="chezmoi init --source-path ~/.Profile"
+alias czl='chezmoi status'
+alias czi='chezmoi init'
+alias cza='chezmoi add'
+alias czar='chezmoi re-add'
+alias czs='chezmoi apply'
 
 source /usr/share/bash-completion/helpers/complete_alias
 complete -F _complete_alias "${!BASH_ALIASES[@]}"
